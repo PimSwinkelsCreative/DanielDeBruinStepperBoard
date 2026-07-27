f2198d11d95f2501db841138e115cd28948b34f6
PIMS_LAPTOP\pim_m <contact@pimswinkels.com>
2026-07-27 14:05:46 +0200

    Added the Arduino IDE compatible versions

---
+++ Arduino IDE versions/Stepper_Motor_Control_Board_Microphone_lift/pinout.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define STEPPER_EN 1
+#define STEPPER_STEP 2
+#define STEPPER_DIR 3
+#define STEPPER_MS1 4
+#define STEPPER_MS2 5
+#define STEPPER_TYPE 6
+#define STEPPER_TX 7
+#define STEPPER_RX 8
+#define DIRECTION_N 9
+#define SW1_N 10
+#define SW2_N 11
+#define SPEED_N 12
+#define MANUAL_CTRL_N 13
